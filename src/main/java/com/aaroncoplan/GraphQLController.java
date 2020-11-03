3cfa759373dc48b4b9bda4cd0bb266d5ac08e00c
AaronCoplan <afcoplan@gmail.com>
2020-11-02 22:08:57 -0500

    Adding formatting.

--- src/main/java/com/aaroncoplan/GraphQLController.java
+++ src/main/java/com/aaroncoplan/GraphQLController.java
@@ -2,38 +2,43 @@ package com.aaroncoplan;
 
 import graphql.ExecutionInput;
 import graphql.GraphQL;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 @Controller
 public class GraphQLController {
 
-    @Autowired
-    private GraphQL graphQL;
+  @Autowired
+  private GraphQL graphQL;
 
-    @PostMapping("/graphql")
-    @ResponseBody
-    public Map<String, Object> executeOperation(@RequestBody Map<String, Object> body) {
-        String query = (String) body.get("query");
-        var castedVariables = (Map<String, Object>) body.get("variables");
-        var variables = castedVariables != null ? castedVariables : new LinkedHashMap<String, Object>();
+  @PostMapping("/graphql")
+  @ResponseBody
+  public Map<String, Object> executeOperation(
+    @RequestBody Map<String, Object> body
+  ) {
+    String query = (String) body.get("query");
+    var castedVariables = (Map<String, Object>) body.get("variables");
+    var variables = castedVariables != null
+      ? castedVariables
+      : new LinkedHashMap<String, Object>();
 
-        var executionInput = ExecutionInput.newExecutionInput(query).variables(variables);
-        var executionResult = graphQL.execute(executionInput);
+    var executionInput = ExecutionInput
+      .newExecutionInput(query)
+      .variables(variables);
+    var executionResult = graphQL.execute(executionInput);
 
-        var result = new LinkedHashMap<String, Object>();
-        result.put("data", executionResult.getData());
+    var result = new LinkedHashMap<String, Object>();
+    result.put("data", executionResult.getData());
 
-        if(!executionResult.getErrors().isEmpty()) {
-            result.put("errors", executionResult.getErrors());
-        }
-
-        return result;
+    if (!executionResult.getErrors().isEmpty()) {
+      result.put("errors", executionResult.getErrors());
     }
+
+    return result;
+  }
 }
--- src/main/java/com/aaroncoplan/GraphQLSchemaProvider.java
+++ src/main/java/com/aaroncoplan/GraphQLSchemaProvider.java
@@ -1,52 +1,101 @@
 package com.aaroncoplan;
 
+import static graphql.Scalars.GraphQLID;
+import static graphql.Scalars.GraphQLString;
+
 import graphql.GraphQL;
 import graphql.schema.*;
 import graphql.schema.idl.*;
+import java.util.HashMap;
+import javax.annotation.PostConstruct;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
-import java.util.HashMap;
-
-import static graphql.Scalars.GraphQLID;
-import static graphql.Scalars.GraphQLString;
 
 @Component
 public class GraphQLSchemaProvider {
 
-    private GraphQL graphQL;
+  private GraphQL graphQL;
 
-    @Bean
-    public GraphQL graphQL() {
-        return this.graphQL;
-    }
+  @Bean
+  public GraphQL graphQL() {
+    return this.graphQL;
+  }
 
-    @PostConstruct
-    public void init() {
-        var bookTypeDefinition = GraphQLObjectType.newObject().name("Book")
-                .field(GraphQLFieldDefinition.newFieldDefinition().name("id").type(GraphQLID))
-                .field(GraphQLFieldDefinition.newFieldDefinition().name("name").type(GraphQLString).build())
-                .field(GraphQLFieldDefinition.newFieldDefinition().name("author").type(GraphQLTypeReference.typeRef("Author")).build())
-                .build();
+  @PostConstruct
+  public void init() {
+    var bookTypeDefinition = GraphQLObjectType
+      .newObject()
+      .name("Book")
+      .field(
+        GraphQLFieldDefinition.newFieldDefinition().name("id").type(GraphQLID)
+      )
+      .field(
+        GraphQLFieldDefinition
+          .newFieldDefinition()
+          .name("name")
+          .type(GraphQLString)
+          .build()
+      )
+      .field(
+        GraphQLFieldDefinition
+          .newFieldDefinition()
+          .name("author")
+          .type(GraphQLTypeReference.typeRef("Author"))
+          .build()
+      )
+      .build();
 
-        var authorTypeDefinition = GraphQLObjectType.newObject().name("Author")
-                .field(GraphQLFieldDefinition.newFieldDefinition().name("id").type(GraphQLID).build())
-                .field(GraphQLFieldDefinition.newFieldDefinition().name("name").type(GraphQLString).build())
-                .build();
+    var authorTypeDefinition = GraphQLObjectType
+      .newObject()
+      .name("Author")
+      .field(
+        GraphQLFieldDefinition
+          .newFieldDefinition()
+          .name("id")
+          .type(GraphQLID)
+          .build()
+      )
+      .field(
+        GraphQLFieldDefinition
+          .newFieldDefinition()
+          .name("name")
+          .type(GraphQLString)
+          .build()
+      )
+      .build();
 
-        var queryTypeDefinition = GraphQLObjectType.newObject().name("Query")
-                .field(GraphQLFieldDefinition.newFieldDefinition().name("books")
-                        .type(GraphQLList.list(GraphQLTypeReference.typeRef("Book"))).build()).build();
+    var queryTypeDefinition = GraphQLObjectType
+      .newObject()
+      .name("Query")
+      .field(
+        GraphQLFieldDefinition
+          .newFieldDefinition()
+          .name("books")
+          .type(GraphQLList.list(GraphQLTypeReference.typeRef("Book")))
+          .build()
+      )
+      .build();
 
-        var codeRegistry = GraphQLCodeRegistry.newCodeRegistry().dataFetcher(FieldCoordinates.coordinates("Query", "books"), (DataFetchingEnvironment dataFetchingEnvironment) -> {
-            var data = new HashMap<String, Object>();
-            data.put("id", 1);
-            data.put("name", "Harry Potter");
-            return new Object[]{data};
-        }).build();
+    var codeRegistry = GraphQLCodeRegistry
+      .newCodeRegistry()
+      .dataFetcher(
+        FieldCoordinates.coordinates("Query", "books"),
+        (DataFetchingEnvironment dataFetchingEnvironment) -> {
+          var data = new HashMap<String, Object>();
+          data.put("id", 1);
+          data.put("name", "Harry Potter");
+          return new Object[] { data };
+        }
+      )
+      .build();
 
-        var schema = GraphQLSchema.newSchema().query(queryTypeDefinition).additionalType(bookTypeDefinition).additionalType(authorTypeDefinition).codeRegistry(codeRegistry).build();
-        this.graphQL = GraphQL.newGraphQL(schema).build();
-    }
+    var schema = GraphQLSchema
+      .newSchema()
+      .query(queryTypeDefinition)
+      .additionalType(bookTypeDefinition)
+      .additionalType(authorTypeDefinition)
+      .codeRegistry(codeRegistry)
+      .build();
+    this.graphQL = GraphQL.newGraphQL(schema).build();
+  }
 }
