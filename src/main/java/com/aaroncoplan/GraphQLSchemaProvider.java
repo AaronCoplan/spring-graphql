509f2957edcf9bd174c548cae58f0648ba42ec75
AaronCoplan <afcoplan@gmail.com>
2020-11-02 22:22:07 -0500

    Add more resolvers.

--- src/main/java/com/aaroncoplan/GraphQLSchemaProvider.java
+++ src/main/java/com/aaroncoplan/GraphQLSchemaProvider.java
@@ -5,8 +5,8 @@ import static graphql.Scalars.GraphQLString;
 
 import graphql.GraphQL;
 import graphql.schema.*;
-import graphql.schema.idl.*;
 import java.util.HashMap;
+import java.util.Map;
 import javax.annotation.PostConstruct;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
@@ -27,7 +27,10 @@ public class GraphQLSchemaProvider {
       .newObject()
       .name("Book")
       .field(
-        GraphQLFieldDefinition.newFieldDefinition().name("id").type(GraphQLID)
+        GraphQLFieldDefinition
+          .newFieldDefinition()
+          .name("id")
+          .type(GraphQLNonNull.nonNull(GraphQLID))
       )
       .field(
         GraphQLFieldDefinition
@@ -52,7 +55,7 @@ public class GraphQLSchemaProvider {
         GraphQLFieldDefinition
           .newFieldDefinition()
           .name("id")
-          .type(GraphQLID)
+          .type(GraphQLNonNull.nonNull(GraphQLID))
           .build()
       )
       .field(
@@ -71,7 +74,22 @@ public class GraphQLSchemaProvider {
         GraphQLFieldDefinition
           .newFieldDefinition()
           .name("books")
-          .type(GraphQLList.list(GraphQLTypeReference.typeRef("Book")))
+          .type(
+            GraphQLNonNull.nonNull(
+              GraphQLList.list(GraphQLTypeReference.typeRef("Book"))
+            )
+          )
+          .build()
+      )
+      .field(
+        GraphQLFieldDefinition
+          .newFieldDefinition()
+          .name("authors")
+          .type(
+            GraphQLNonNull.nonNull(
+              GraphQLList.list(GraphQLTypeReference.typeRef("Author"))
+            )
+          )
           .build()
       )
       .build();
@@ -79,11 +97,35 @@ public class GraphQLSchemaProvider {
     var codeRegistry = GraphQLCodeRegistry
       .newCodeRegistry()
       .dataFetcher(
+        FieldCoordinates.coordinates("Book", "author"),
+        (DataFetchingEnvironment dataFetchingEnvironment) -> {
+          var source = (Map<String, Object>) dataFetchingEnvironment.getSource();
+          var id = (Integer) source.get("id");
+          if (id != 1) {
+            return null;
+          }
+
+          var data = new HashMap<String, Object>();
+          data.put("id", 2);
+          data.put("name", "Suzanne Collins");
+          return data;
+        }
+      )
+      .dataFetcher(
         FieldCoordinates.coordinates("Query", "books"),
         (DataFetchingEnvironment dataFetchingEnvironment) -> {
           var data = new HashMap<String, Object>();
           data.put("id", 1);
-          data.put("name", "Harry Potter");
+          data.put("name", "Hunger Games");
+          return new Object[] { data };
+        }
+      )
+      .dataFetcher(
+        FieldCoordinates.coordinates("Query", "authors"),
+        (DataFetchingEnvironment dataFetchingEnvironment) -> {
+          var data = new HashMap<String, Object>();
+          data.put("id", 2);
+          data.put("name", "Suzanne Collins");
           return new Object[] { data };
         }
       )
